bfaed2110f96aba86243463ff648b0c0104e44cb
surya <eksurya27@gmail.com>
2023-09-29 11:33:33 +0530

    Added files

---
+++ test1.py
@@ -0,0 +1,10 @@
+import time
+from selenium import webdriver
+
+options = webdriver.ChromeOptions()
+options.add_experimental_option("detach",True)
+driver=webdriver.Chrome(options=options)
+
+
+driver.get("http://selenium.dev")
+
